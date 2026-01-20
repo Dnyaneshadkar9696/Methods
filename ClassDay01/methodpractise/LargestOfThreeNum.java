fedcf46aab131adca78235ce8ef4681d6e470e12
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-20 20:04:48 +0530

    Method Practice

---
+++ ClassDay01/methodpractise/LargestOfThreeNum.java
@@ -0,0 +1,19 @@
+package methodpractise;
+
+public class LargestOfThreeNum {
+	
+	public void largeNum(int a, int b , int c) {
+		
+		if(a>b  && a>c) {
+			System.out.println(a + " is the largest number");
+		}
+		else if(b>a && b>c) {
+			System.out.println(b + " is the largest number");
+		}
+		else {
+			System.out.println(c + " is the largest number");
+		}
+		
+	}
+
+}
