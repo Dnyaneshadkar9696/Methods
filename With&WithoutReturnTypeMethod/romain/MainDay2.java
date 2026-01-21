293d7f9757ba565aedfc3d0db57fdb7dd0ef07b4
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-21 22:57:19 +0530

    Update MainDay2.java

--- With&WithoutReturnTypeMethod/romain/MainDay2.java
+++ With&WithoutReturnTypeMethod/romain/MainDay2.java
@@ -16,7 +16,28 @@ public class MainDay2 {
 		my.checkEvenOdd();
 		my.multiplicationTable();
 		my.stringnewLine();
+
+		System.out.println();
+		System.out.println("Starting of with return type methods");
+		WithReturnType with = new WithReturnType();
+		int a = with.theAddition();
+		System.out.println("Addition is : "+ a);
+		int w = with.theSquare();
+		System.out.println("Square is : "+ w);
+		boolean ds = with.checkEvenOdd();
+		System.out.println("Number is Even : "+ ds);
+		int u = with.greatNumber();
+		System.out.println("Greatest Number is : "+ u);
+		int rt = with.stringlength();
+		System.out.println("Length is : "+ rt);
+		String cd = with.drivingEligibility();
+		System.out.println("Eligibility : " + cd);
+		String der = with.checkResult();
+		System.out.println("Result : "+ der);
+		int xd = with.theProduct();
+		System.out.println("The product of numbers is : "+ xd);
 	}
 
 }
+
 
