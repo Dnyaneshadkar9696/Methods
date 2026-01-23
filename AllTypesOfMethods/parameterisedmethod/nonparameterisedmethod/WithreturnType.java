3197bd1240d9498c2f73847226fd41ea110c6e32
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-23 07:12:12 +0530

    Implement multiple methods in WithreturnType class
    Added multiple methods to perform various operations, including addition, name retrieval, age checking, mobile number retrieval, symbol giving, and percentage retrieval.

--- AllTypesOfMethods/parameterisedmethod/nonparameterisedmethod/WithreturnType.java
+++ AllTypesOfMethods/parameterisedmethod/nonparameterisedmethod/WithreturnType.java
@@ -2,4 +2,39 @@ package day3.nonparameterisedmethod;
 
 public class WithreturnType {
 
+  public int addition() {
+		int a = 34;
+		int b = 33; 
+	     return a+b;
+	}
+	
+	public String Myname() {
+		return "Dnyanesh";
+	}
+	
+	public boolean checkAge() {
+		int a = 18;
+		if(a < 18) {
+			return false;
+		}
+		else 
+		{
+			return true;
+		}
+	}
+	
+	public long NumberMob() {
+		return 2342343543l;
+	}
+	
+	public char giveSymbol() {
+		return '$';
+	}
+	
+	public float percentageMy() {
+		float per = 87.4f;
+		return per;
+	}
+	
 }
+
